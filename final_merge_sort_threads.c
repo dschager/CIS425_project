dfe123278922fee745adac7aaa70f2e2b598a65b
Daniel Schager <149520413+dschager@users.noreply.github.com>
2025-02-17 13:16:55 -0500

    Update final_merge_sort_threads.c

--- final_merge_sort_threads.c
+++ final_merge_sort_threads.c
@@ -1,5 +1,14 @@
-//
+/* 
+Daniel Schager and Daniel Jackson
+CIS425
+19 February 2024
+Project 1
+*/
+
 /* Iterative C program for merge sort */
+// Obtained from https://www.geeksforgeeks.org/iterative-merge-sort/
+//Please Note: All original comments were removed to improve readability
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
@@ -9,7 +18,7 @@ void merge(int arr[], int l, int m, int r);
 
 int min(int x, int y) { return (x<y)? x :y; }
 
-//Creates a pointer an array, along with left, mid, and right
+//Creates a pointer to an array, along with left, mid, and right
 typedef struct {
     int *arr;
     int left;
@@ -17,6 +26,7 @@ typedef struct {
     int right;
 } MergeParams;  
 
+//runner function
 void *mergeThread(void *args) {
 	MergeParams *params = (MergeParams *) args;
 	
@@ -27,20 +37,16 @@ void *mergeThread(void *args) {
 
 
 
-/* Iterative mergesort function to sort arr[0...n-1] */
+
 void mergeSort(int arr[], int n)
 {
-int curr_size; // For current size of subarrays to be merged
-				// curr_size varies from 1 to n/2
-int left_start; // For picking starting index of left subarray
-				// to be merged
+int curr_size; 
+int left_start; 
 
 int num_threads;
 pthread_t threads[5]; //Declares up to 5 threads
 
-// Merge subarrays in bottom up manner. First merge subarrays of
-// size 1 to create sorted subarrays of size 2, then merge subarrays
-// of size 2 to create sorted subarrays of size 4, and so on.
+
 for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size)
 {
     num_threads = 0;
@@ -54,14 +60,14 @@ for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size)
 
         //Stops creating threads after max exceeded
         if (num_threads < 5) {
-            //Creates a structure to hold same parameters as original method
+            //Creates a structure to hold the same parameters as the original method
             MergeParams *params = (MergeParams *) malloc(sizeof(MergeParams));
             params->arr = arr;
             params->left = left_start;
             params->mid = mid;
             params->right = right_end;
 
-			//Creates a thread to perform merge
+		//Creates a thread to perform merge
             pthread_create(&threads[num_threads], NULL, mergeThread, params);
             num_threads++;
         } else {
@@ -71,14 +77,13 @@ for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size)
 
 		
 	}
-	//joins threads together
+	//loops and joins threads together
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
     }
 }
 }
 
-/* Function to merge the two haves arr[l..m] and arr[m+1..r] of array arr[] */
 void merge(int arr[], int l, int m, int r)
 {
 	int i, j, k;
@@ -88,7 +93,7 @@ void merge(int arr[], int l, int m, int r)
 	int L[n1], R[n2];
 
 	//int *L = (int *)malloc (n1 * sizeof(int));
-    //int *R = (int *)malloc (n2 * sizeof(int));
+    	//int *R = (int *)malloc (n2 * sizeof(int));
 	
 
 	for (i = 0; i < n1; i++)
@@ -96,7 +101,6 @@ void merge(int arr[], int l, int m, int r)
 	for (j = 0; j < n2; j++)
 		R[j] = arr[m + 1+ j];
 
-	/* Merge the temp arrays back into arr[l..r]*/
 	i = 0;
 	j = 0;
 	k = l;
@@ -115,7 +119,7 @@ void merge(int arr[], int l, int m, int r)
 		k++;
 	}
 
-	/* Copy the remaining elements of L[], if there are any */
+	
 	while (i < n1)
 	{
 		arr[k] = L[i];
@@ -123,7 +127,7 @@ void merge(int arr[], int l, int m, int r)
 		k++;
 	}
 
-	/* Copy the remaining elements of R[], if there are any */
+
 	while (j < n2)
 	{
 		arr[k] = R[j];
@@ -132,7 +136,7 @@ void merge(int arr[], int l, int m, int r)
 	}
 }
 
-/* Function to print an array */
+
 void printArray(int A[], int size)
 {
 	int i;
@@ -141,18 +145,21 @@ void printArray(int A[], int size)
 	printf("\n");
 }
 
-/* Driver program to test above functions */
+
 int main()
 {
-
+	//Creates a clock with start' and 'end' variables
 	clock_t start, end;
 	double cpu_time_used;
 	start = clock();
 
-	//size of array
+	//n = size of array
 	int n = 10000;
+	
+	//4 declared arrays
 	int arr1[n], arr2[n], arr3[n], arr4[n];
 
+	//populates arrays with random integers 1-999
 	int i;
 	for (i=0; i< 10000; i++) {
 		arr1[i] = rand() % 1000;
@@ -161,16 +168,18 @@ int main()
 		arr4[i] = rand() % 1000;
 	}
 
+	/*
 	int n1 = sizeof(arr1)/sizeof(arr1[0]);
 	int n2 = sizeof(arr2)/sizeof(arr2[0]);
 	int n3 = sizeof(arr3)/sizeof(arr3[0]);
 	int n4 = sizeof(arr4)/sizeof(arr4[0]);
+	*/
 
 	mergeSort(arr1, n);
 	mergeSort(arr2, n);
 	mergeSort(arr3, n);
 	mergeSort(arr4, n);
-	printf("All arrays complete");
+	printf("All arrays complete\n");
 
 	end = clock();
 	cpu_time_used = ((double) (end-start)) / CLOCKS_PER_SEC;
